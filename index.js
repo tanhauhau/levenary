e3143b736a1075ad05e93d4a1bab2028a7383824
Huáng Jùnliàng <jlhwung@gmail.com>
2019-12-24 22:27:44 -0500

    chore: assume array in for-of transform

--- index.js
+++ index.js
@@ -12,32 +12,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function levenArray(str, array) {
   var minLeven = Number.POSITIVE_INFINITY;
   var result = undefined;
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
 
-  try {
-    for (var _iterator = array[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var item = _step.value;
-      var distance = (0, _leven.default)(str, item);
+  for (var _i2 = 0; _i2 < array.length; _i2++) {
+    var item = array[_i2];
+    var distance = (0, _leven.default)(str, item);
 
-      if (distance < minLeven) {
-        minLeven = distance;
-        result = item;
-      }
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return != null) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
+    if (distance < minLeven) {
+      minLeven = distance;
+      result = item;
     }
   }
 
